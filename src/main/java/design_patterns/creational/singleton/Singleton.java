dc3ba8af5b645f02968527fb984d43580fad0060
Timofey_Podvorny <Timofey_Podvorny@epam.com>
2021-04-05 18:23:26 +0300

    Singleton pattern

---
+++ src/main/java/design_patterns/creational/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package design_patterns.creational.singleton;
+
+public class Singleton {
+
+  private static Singleton instance;
+
+  private Singleton() {}
+
+  public static Singleton getInstance() {
+    synchronized (Singleton.class) {
+      if (instance == null) {
+        instance = new Singleton();
+      }
+    }
+    return instance;
+  }
+}
---
+++ src/test/java/design_patterns/creational/singleton/SingletonTest.java
@@ -0,0 +1,15 @@
+package design_patterns.creational.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SingletonTest {
+
+  @Test
+  public void testSingularity() {
+    Singleton firstRef = Singleton.getInstance();
+    Singleton secondRef = Singleton.getInstance();
+    assertEquals(firstRef, secondRef);
+  }
+}
